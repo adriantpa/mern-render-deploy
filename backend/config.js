7b7fad420484aca255dae04c6c1b9f0e472a644f
adriantpa <adrianltrc@yahoo.com>
2023-11-23 14:05:20 -0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+export const DB_HOST = process.env.FRONTEND_URL || "localhost";
+export const DB_DATABASE = process.env.DB_DATABASE || "postgres";
+export const DB_USER = process.env.DB_USER || "postgres";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "mysecret";
+export const DB_PORT = process.env.DB_PORT || 5440;
+
+export const PORT = process.env.PORT || 3000;
